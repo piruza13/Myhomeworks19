2d35e79e9718916d6f47e15506e0b1a0f382e571
Piruza <piruza.sargsyan@buymie.eu>
2023-09-09 21:51:27 +0400

    Test

---
+++ src/TheLargest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class TheLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First number is: ");
+        int a = sc.nextInt();
+
+        Scanner sca = new Scanner(System.in);
+        System.out.println("Second number is: ");
+        int b = sc.nextInt();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Third number is: ");
+        int c = sc.nextInt();
+
+        if (a > b && a > c) {
+            System.out.println("The largest number is:" + a);
+        } else if (b > a && b > c) {
+            System.out.println("The largest number is:" + b);
+        } else {
+            System.out.println("The largest number is:" + c);
+        }
+    }
+}
