5fadf3069cc071feaf265eba131edc5778c49b3b
Piruza <piruza.sargsyan@buymie.eu>
2023-09-12 01:12:16 +0400

    Update

---
+++ src/Products.java
@@ -0,0 +1,16 @@
+
+public class Products
+{
+    private String _name;
+
+    public Products(String item_name)
+    {
+        this._name = item_name;
+    }
+    public String get_name()
+    {
+        return _name;
+    }
+}
+
+
