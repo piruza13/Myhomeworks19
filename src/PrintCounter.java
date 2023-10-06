7735f814a9b67dba8584f2349fd24c775286cd18
piruza <piruza.sargsyan@buymie.eu>
2023-10-06 13:56:55 +0100

    update

---
+++ src/PrintCounter.java
@@ -0,0 +1,8 @@
+public class PrintCounter {
+    public static void main(String[] args) {
+        StaticExample exmp1 = new StaticExample();
+        StaticExample exmp2 = new StaticExample();
+        StaticExample exmp3 = new StaticExample();
+        System.out.println(StaticExample.getCounter());
+    }
+}
