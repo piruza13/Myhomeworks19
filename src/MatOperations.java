1f00ccb9d1fc78540b3a75a7044c4f0708f4dd58
piruza <piruza.sargsyan@buymie.eu>
2023-10-06 14:18:28 +0100

    update

---
+++ src/MatOperations.java
@@ -0,0 +1,10 @@
+public class MatOperations {
+
+    public static int getSquare (int a) {
+        return a*a;
+    }
+    public static int getCube (int b) {
+        return b*b*b;
+    }
+}
+
---
+++ src/Print4.java
@@ -0,0 +1,6 @@
+public class Print4 {
+    public static void main(String[] args) {
+        System.out.println(MatOperations.getSquare(6));
+        System.out.println(MatOperations.getCube(2));
+    }
+}
