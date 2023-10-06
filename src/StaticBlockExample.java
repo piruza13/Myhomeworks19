a5a27df96c9c2467700a9652d8961b80fa4b1f47
piruza <piruza.sargsyan@buymie.eu>
2023-10-06 12:45:36 +0100

    update

---
+++ src/StaticBlockExample.java
@@ -0,0 +1,12 @@
+public class StaticBlockExample {
+
+     static {
+
+        System.out.println("Static block is invoked");
+    }
+
+    public static void main(String[] args) {
+        StaticBlockExample staticblock = new StaticBlockExample();
+
+    }
+}
