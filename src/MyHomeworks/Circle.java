1dd63f6e65d4259e9f3b56950151b836a949ec43
Piruza <piruza.sargsyan@buymie.eu>
2023-10-08 00:36:19 +0400

    Update

---
+++ src/MyHomeworks/Circle.java
@@ -0,0 +1,10 @@
+package MyHomeworks;
+
+public class Circle {
+    public static float calculateArea (int radius) {
+        int diameter = 2 * radius;
+        float pi = 3.14F;
+        float area = pi * diameter;
+        return area;
+    }
+}
---
+++ src/MyHomeworks/NewClass.java
@@ -0,0 +1,7 @@
+import static MyHomeworks.Circle.*;
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println(calculateArea(12));
+    }
+}
